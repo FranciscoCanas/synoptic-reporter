9850a19179acb0e42ce9525005482e59587dcfd5
FranciscoCanas <mailfrancisco@gmail.com>
2015-01-30 16:45:07 -0500

    Experimenting with templates and controllers.

--- demo/js/controllers.js
+++ demo/js/controllers.js
@@ -1,21 +1,26 @@
 var controllers = angular.module('controllers', []);
 
-controllers.controller('musicController', function ($scope) {
-	$scope.songs = [
-		{ 	'id': 0,
-			'name': 'Stephen Foster',
-			'song': 'Oh! Susanna',
-			'url': 'PZXvxOXXdCk'
-			},
-		{ 	'id':1,
-			'name': 'Dink Roberts',
-			'song': 'Coo Coo',
-			'url': 'GkyZ555DHrI'
-			},
-		{	'id': 2,
-			'name': 'Blind Lemon Jefferson',
-			'song': 'Black Snake Moan',
-			'url': 'h3yd-c91ww8'
-			},
-	];
+controllers.controller('musicController', function ($scope, $parse) {
+	$scope.radio = {
+		'heading': 'Favourite Song',
+		'display': $parse('item.song + \' by \' + item.name'),
+		'choices':
+		[
+			{ 	'id': 0,
+				'name': 'Stephen Foster',
+				'song': 'Oh! Susanna',
+				'url': 'PZXvxOXXdCk'
+				},
+			{ 	'id':1,
+				'name': 'Dink Roberts',
+				'song': 'Coo Coo',
+				'url': 'GkyZ555DHrI'
+				},
+			{	'id': 2,
+				'name': 'Blind Lemon Jefferson',
+				'song': 'Black Snake Moan',
+				'url': 'h3yd-c91ww8'
+				},
+		]
+	}
 });
