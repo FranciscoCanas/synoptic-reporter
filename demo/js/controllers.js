6324eabf09ebdba6eb4e769021a20b5c06f8edcc
FranciscoCanas <mailfrancisco@gmail.com>
2015-01-30 15:19:33 -0500

    Added controller example.

--- demo/js/controllers.js
+++ demo/js/controllers.js
@@ -1,1 +1,21 @@
 var controllers = angular.module('controllers', []);
+
+controllers.controller('musicController', function ($scope) {
+	$scope.songs = [
+		{ 	'id': 0,
+			'name': 'Stephen Foster',
+			'song': 'Oh! Susanna',
+			'url': 'PZXvxOXXdCk'
+			},
+		{ 	'id':1,
+			'name': 'Dink Roberts',
+			'song': 'Coo Coo',
+			'url': 'GkyZ555DHrI'
+			},
+		{	'id': 2,
+			'name': 'Blind Lemon Jefferson',
+			'song': 'Black Snake Moan',
+			'url': 'h3yd-c91ww8'
+			},
+	];
+});
