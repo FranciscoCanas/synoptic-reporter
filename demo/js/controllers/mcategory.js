ce8b8870f6510d7c436a2588d54e52eb95a7b8b3
FranciscoCanas <mailfrancisco@gmail.com>
2015-02-14 16:09:56 -0500

    Added layout for mcat.

--- demo/js/controllers/mcategory.js
+++ demo/js/controllers/mcategory.js
@@ -1,2 +1,8 @@
 angular.module('controllers').controller('mcategoryController', function ($scope) {
+	$scope.nodes = [
+		{'label':'Adrenals'},
+		{'label':'Liver'},
+		{'label':'Bone'},
+		{'label':'Other'},
+	]
 });
