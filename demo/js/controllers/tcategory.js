8b9937cad2148afa414da473cc1a16f85007be4e
FranciscoCanas <mailfrancisco@gmail.com>
2015-02-15 19:00:30 -0500

    TCat section, plus various layout/ui edits.

--- demo/js/controllers/tcategory.js
+++ demo/js/controllers/tcategory.js
@@ -1,2 +1,23 @@
 angular.module('controllers').controller('tcategoryController', function ($scope) {
+	$scope.structures = [
+		{'label': 'Pleura'},
+		{'label': 'Brachial Plexus'},
+		{'label': 'Diaphragm'},
+		{'label': 'Mediastinal fat'},
+		{'label': 'Aorta'},
+		{'label': 'Pericardium or Heart'},
+		{'label': 'Mediastinal Vessels'},
+		{'label': 'Trachea'},
+		{'label': 'Esophagus'},
+		{'label': 'Trachea esophageal groove'},
+		{'label': 'Vertebral Body'},
+		{'label': 'hest wall & Ribs'},
+	];
+
+	$scope.nodules=[
+		{'label':'In the same lobe'},
+		{'label':'In a different lobe, same lung'},
+		{'label':'In the opposite lung (M1a)'},
+	];
 });
+
