0bf427fd48d546b07c65924f149f91b106d81487
FranciscoCanas <mailfrancisco@gmail.com>
2015-03-02 17:37:11 -0500

    Fixes node checklist template

--- demo/js/controllers/index.js
+++ demo/js/controllers/index.js
@@ -1,30 +1,4 @@
 angular.module('controllers').controller('indexController', function ($scope, $location, $anchorScroll, $document, $parse) {
-	$scope.test = {
-		heading: 'Testing!'
-	}
-	$scope.tester = {
-		heading: 'Favourite Song',
-		display: $parse('item.song + \' by \' + item.name'),
-		selection: 0,
-		choices:
-		[
-		{ 	'id': 0,
-		'name': 'Stephen Foster',
-		'song': 'Oh! Susanna',
-		'url': 'PZXvxOXXdCk'
-	},
-	{ 	'id':1,
-	'name': 'Dink Roberts',
-	'song': 'Coo Coo',
-	'url': 'GkyZ555DHrI'
-},
-{	'id': 2,
-'name': 'Blind Lemon Jefferson',
-'song': 'Black Snake Moan',
-'url': 'h3yd-c91ww8'
-},
-]
-};
 
 /* Scrolls to specified section id. */
 $scope.goToSection = function(id) {
@@ -93,55 +67,4 @@ $scope.goToSection = function(id) {
 		'file': 'impressions',
 	},
 	];
-});
-
-angular.module('controllers').controller('musicController', function ($scope, $parse) {
-	$scope.radio = {
-		'heading': 'Favourite Song',
-		'display': $parse('item.song + \' by \' + item.name'),
-		'selection': 0,
-		'choices':
-		[
-		{ 	'id': 0,
-		'name': 'Stephen Foster',
-		'song': 'Oh! Susanna',
-		'url': 'PZXvxOXXdCk'
-	},
-	{ 	'id':1,
-	'name': 'Dink Roberts',
-	'song': 'Coo Coo',
-	'url': 'GkyZ555DHrI'
-},
-{	'id': 2,
-'name': 'Blind Lemon Jefferson',
-'song': 'Black Snake Moan',
-'url': 'h3yd-c91ww8'
-},
-]
-};
-});
-
-angular.module('controllers').controller('filmController', function ($scope, $parse) {
-	$scope.checkbox = {
-		'heading': 'Have you watched any of the following films?',
-		'display': $parse('item.film'),
-		'choices':
-		[
-		{ 	'id': 0,
-		'film': 'Reefer Madness',
-		'url': 'PZXvxOXXdCk',
-		'on': false,
-	},
-	{ 	'id':1,
-	'film': 'Plan 9 From Outer Space',
-	'url': 'GkyZ555DHrI',
-	'on': false,
-},
-{	'id': 2,
-'film': 'Manos: The Hands of Fate',
-'url': 'h3yd-c91ww8',
-'on': false,
-},
-],
-};
 });
